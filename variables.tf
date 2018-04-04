b3129b4f3dff61d8f9501ee0ea4f9014a6dcd307
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-04-04 14:32:39 +0700

    uppercase rpc

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "product_domain" {
   type        = "string"
-  description = "The name of the product domain which this rpc belongs to"
+  description = "The name of the product domain which this RPC belongs to"
 }
 
 variable "cluster" {
   type        = "string"
-  description = "The name of the cluster which this rpc belongs to"
+  description = "The name of the cluster which this RPC belongs to"
 }
